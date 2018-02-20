d97a56cf0002977abfd43d7b7009d011123e4af3
Chinmay Chandak <chandakchinmay@gmail.com>
2018-02-21 00:45:13 +0530

    Added 1st prac of AOA
    list of program added
    - insert-selection.c
    
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

--- OS/P02-program.sh
+++ OS/P02-program.sh
@@ -13,6 +13,9 @@ echo SHELL: $SHELL
 echo home dir: $HOME
 echo OS: $(lsb_release -is)
 echo current path: $(pwd)
+echo
 
 # Q3:
-echo  OS Version: $()
+echo OS Version: $()
+echo Release number: $(lsb_release -rs)
+echo kernel version: $(uname -r)

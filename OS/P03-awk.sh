4ac5cd53f74e3f3b74270974adf14b36580e308f
Chinmay Chandak <chandakchinmay@gmail.com>
2018-02-21 01:09:21 +0530

    Added 3rd prac of OS
    list of program added
    - awk
    - displayRange
    - sortWrite
    - exp3.c
    
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

---
+++ OS/P03-awk.sh
@@ -0,0 +1,7 @@
+awk '{print $1 }' grades.txt
+
+echo
+echo
+
+cat grades.txt | awk '{print $2}'
+
---
+++ OS/P03-sortWrite.sh
@@ -0,0 +1,3 @@
+sort data.txt | cat >> output.txt
+
+# sort the data from file and write into another file
